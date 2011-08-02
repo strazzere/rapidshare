8b60679216adfd9653309a20dbaa9a7a85e5531d
Lukas Stejskal <lucastej@gmail.com>
2011-08-02 22:34:39 +0200

    fix Rapidshare::FILE_REGEXP to accept https protocol

--- lib/rapidshare/rapidshare.rb
+++ lib/rapidshare/rapidshare.rb
@@ -1,6 +1,6 @@
 class Rapidshare
 
-  FILE_REGEXP = /\Ahttp:\/\/rapidshare.com\/files\/([0-9]+)\/(.+)\z/ix unless defined?(FILE_REGEXP)
+  FILE_REGEXP = /\Ahttps?:\/\/rapidshare.com\/files\/([0-9]+)\/(.+)\z/ix unless defined?(FILE_REGEXP)
 
   def self.build_file_url(server_id, short_host, file_id, file_name)
     "http://rs#{server_id}#{short_host}.rapidshare.com/files/#{file_id}/#{file_name}"
