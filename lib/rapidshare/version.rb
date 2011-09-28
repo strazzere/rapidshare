18ff567e2b3d42b46a8b186d94a70b9b3b27aa59
Lukas Stejskal <lucastej@gmail.com>
2011-09-28 21:57:53 +0200

    move changelog to comments included in Rapidshare module

--- lib/rapidshare/version.rb
+++ lib/rapidshare/version.rb
@@ -1,3 +1,8 @@
 module Rapidshare
   VERSION = "0.4.5"
+
+  # CHANGELOG:
+  # 0.4.5
+  # rewrote download method from Net:HTTP to Curb gem
+  # added download scripts as examples of use of rapidshare gem
 end
