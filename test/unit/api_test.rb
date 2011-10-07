3585276e14cb36c42b6871406240660deafbdf66
Lukas Stejskal <lucastej@gmail.com>
2011-10-08 00:05:51 +0200

    remove obsolete "valid API request" test (too generic, all relevant API requests will be tested)

--- test/unit/api_test.rb
+++ test/unit/api_test.rb
@@ -69,28 +69,7 @@ class ApiTest < Test::Unit::TestCase
     end
   end  
 
-  # TODO move to checkfiles test
-  # 
-  context "Valid API request" do
-    setup do
-      FakeWeb.register_uri(:get,
-        'https://api.rapidshare.com/cgi-bin/rsapi.cgi?sub=checkfiles&files=439727873&filenames=DT-02-DT.rar',
-        :body => '439727873,DT-02-DT.rar,94615872,971,1,l3,F50F440C343749FD7C91286369BED105'
-      )
-    end
-    
-    should "return response" do
-      response = Rapidshare::API.request(:checkfiles, {
-        :files => "439727873",
-        :filenames => "DT-02-DT.rar"
-      })
-      assert response.is_a?(String)
-      assert response.length > 0
-      assert !response.include?("ERROR: ")      
-    end
-  end
-
-  context "Invalid routine call" do
+  context "Invalid method call" do
     setup do
       FakeWeb.register_uri(:get,
         'https://api.rapidshare.com/cgi-bin/rsapi.cgi?sub=invalid_routine&param_1=value_1',
