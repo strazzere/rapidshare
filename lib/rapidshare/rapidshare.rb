0a5806c6f347db9844383fcdece8eecf0ccb94be
Lukas Stejskal <lucastej@gmail.com>
2011-10-08 21:11:26 +0200

    remove unused methods and constants from Rapidshare class

--- lib/rapidshare/rapidshare.rb
+++ lib/rapidshare/rapidshare.rb
@@ -4,12 +4,4 @@
 require 'active_support/core_ext/object/to_query'
 require 'active_support/core_ext/hash/keys'
 
-class Rapidshare
-
-  FILE_REGEXP = /\Ahttps?:\/\/rapidshare.com\/files\/([0-9]+)\/(.+)\z/ix unless defined?(FILE_REGEXP)
-
-  def self.build_file_url(server_id, short_host, file_id, file_name)
-    "https://rs#{server_id}#{short_host}.rapidshare.com/files/#{file_id}/#{file_name}"
-  end
-
-end
+class Rapidshare; end
