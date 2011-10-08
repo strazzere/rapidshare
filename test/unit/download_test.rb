9a34e1a2850624e7032475826a4216fad89eff40
Lukas Stejskal <lucastej@gmail.com>
2011-10-09 01:42:33 +0200

    add tests for download_link and downloaded methods

--- test/unit/download_test.rb
+++ test/unit/download_test.rb
@@ -9,5 +9,28 @@ class DownloadTest < Test::Unit::TestCase
     @file = 'https://rapidshare.com/files/829628035/HornyRhinos.jpg'
     @downloader = Rapidshare::Download.new(@file, @rs.api)
   end
+  
+  # TODO how to test "perform" method? probably by integration testing
+
+  context "download_link method" do
+    should "return link which downloads file by Rapidshare API" do
+      assert_equal @downloader.download_link,
+        'https://rs.rapidshare.com/cgi-bin/rsapi.cgi?cookie=F0EEB41B38363A41F0D125102637DB7236468731F8DB760DC57934B4714C8D13&fileid=&filename=&sub=download'
+    end
+  end
+  
+  context "downloaded? method" do
+    should "have the same value as \"downloaded\" attribute" do
+      assert_equal @downloader.downloaded, @downloader.downloaded?
+    end
+
+    should "return \"false\" by default " do
+      assert ! @downloader.downloaded?
+    end
+
+    should_eventually  "return \"true\" after file was successfully downloaded" do
+      assert true
+    end
+  end
 
 end
