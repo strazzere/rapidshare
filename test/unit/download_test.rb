fe8037228ba4b50f251d836c71cd8960b2f090da
Lukas Stejskal <lucastej@gmail.com>
2011-10-09 13:43:51 +0200

    add tests for Download class: initialize and check method

--- test/unit/download_test.rb
+++ test/unit/download_test.rb
@@ -10,7 +10,68 @@ class DownloadTest < Test::Unit::TestCase
     @downloader = Rapidshare::Download.new(@file, @rs.api)
   end
   
+  context "initialize method" do
+    should "enable overriding filename attribute" do
+      @downloader = Rapidshare::Download.new(@file, @rs.api, :save_as => 'picture_of_rhinos.jpg')
+      assert_equal 'picture_of_rhinos.jpg', @downloader.filename
+    end
+
+    should "enable overriding downloads_dir attribute" do
+      @downloader = Rapidshare::Download.new(@file, @rs.api, :downloads_dir => '/tmp')
+      assert_equal '/tmp', @downloader.downloads_dir
+    end
+  end
+    
+  context "check method for valid file" do
+    setup do
+      @check_result = @downloader.check
+    end
+    
+    should "return true" do
+      assert @check_result
+    end
+    
+    # test if check method assigns all attributes needed for download
+    
+    should "assign fileid attribute" do
+      assert_equal '829628035', @downloader.fileid
+    end
+
+    should "assign filename attribute" do
+      assert_equal 'HornyRhinos.jpg', @downloader.filename
+    end
+
+    should "assign filesize attribute" do
+      assert_instance_of Float, @downloader.filesize
+      assert_equal 272288.0, @downloader.filesize
+    end
+
+    should "assign server_id attribute" do
+      assert_equal '370', @downloader.server_id
+    end
+
+    should "assign short_host attribute" do
+      assert_equal 'l33', @downloader.short_host
+    end
+  end
+
+  context "check method for invalid file" do
+    setup do
+      @downloader = Rapidshare::Download.new('', @rs.api)
+      @check_result = @downloader.check
+    end
+    
+    should "return false" do
+      assert ! @check_result
+    end
+
+    should "return error" do
+      assert ! @downloader.error.to_s.empty?
+    end
+  end
+    
   # TODO how to test "perform" method? probably by integration testing
+  # or should we mock the download as well? or both? (yeah, probably that)
 
   context "download_link method" do
     should "return link which downloads file by Rapidshare API" do
