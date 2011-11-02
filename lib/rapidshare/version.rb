70d279b993956b7872a37738229079dca1ee9522
Lukas Stejskal <lucastej@gmail.com>
2011-11-02 22:03:18 +0100

    release 0.5.0

--- lib/rapidshare/version.rb
+++ lib/rapidshare/version.rb
@@ -1,10 +1,21 @@
 # encoding: utf-8
 
 module Rapidshare
-  VERSION = "0.4.5"
+  VERSION = "0.5.0"
 
   # CHANGELOG:
+  #
+  # 0.5.0
+  # wrote documentation
+  # wrote tests
+  # refactored API class
+  # changed HTTP client for API class: replace HTTParty with Net::HTTPS
+  # removed obsolete Account class
+  # enabled login with cookie only
+  # add API#method_missing for unsupported Rapidshare service calls
+  #
   # 0.4.5
   # rewrote download method from Net:HTTP to Curb gem
   # added download scripts as examples of use of rapidshare gem
+  #
 end
